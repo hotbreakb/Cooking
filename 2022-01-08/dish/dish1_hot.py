ffcfb62bfb2d2e527ffb21ecd2b17261c78d0bbf
hotbreakb <areyhappy12@gmail.com>
2022-01-08 20:16:37 +0900

    2022-01-08: hot

---
+++ 2022-01-08/dish/dish1_hot.py
@@ -0,0 +1,15 @@
+#
+#  3085번: 사탕 게임
+#  https://www.acmicpc.net/problem/3085
+#  Version: Python 3.8.9
+#
+#  Created by hot on 2022/01/08.
+#
+
+
+from sys import stdin
+
+read = stdin.readline
+
+if __name__ == "__main__":
+    pass
