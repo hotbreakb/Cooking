3d610d295bc7bef6f56ff0491c7cee68b294c98c
hotbreakb <areyhappy12@gmail.com>
2022-04-09 14:37:21 +0900

    [PGS] 최소직사각형

--- programmers/hotbreakb/최소직사각형.py
+++ programmers/hotbreakb/최소직사각형.py
@@ -1,7 +1,11 @@
 def solution(sizes):
-    for x in sizes:
-        print(x)
-    return max(max(x) for x in sizes) * max(min(x) for x in sizes)
+    width, height = 0, 0
+    for card in sizes:
+        card = sorted(card)
+        width = max(width, card[0])
+        height = max(height, card[1])
+
+    return width*height
 
 
 sizes = [[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]
