83eccb484158201c2e23b0ca0d67134d7477e897
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-04-19 11:19:04 +0900

    [PGS] 점프와 순간 이동
    https://programmers.co.kr/learn/courses/30/lessons/12980

---
+++ programmers/whitehyun/점프와 순간 이동.py
@@ -0,0 +1,11 @@
+#
+#  점프와 순간 이동
+#  https://programmers.co.kr/learn/courses/30/lessons/12980
+#  Version: Python 3.8.12
+#
+#  Created by WhiteHyun on 2022/04/19.
+#
+
+
+def solution(n):
+    return bin(n).count("1")
